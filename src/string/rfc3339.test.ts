00f4c471a3c90f2c4aadefc166fb1348fc1d6fcf
uzmoi <63585970+uzmoi@users.noreply.github.com>
2025-05-02 23:59:27 +0900

    add RFC3339

---
+++ src/string/rfc3339.test.ts
@@ -0,0 +1,63 @@
+import { describe, expect, test } from "@jest/globals";
+import { formatRFC3339, parseRFC3339 } from "./rfc3339";
+
+describe("parse", () => {
+  test("valid", () => {
+    expect(parseRFC3339("1975-01-01T00:00:00+00:00")).toEqual({
+      year: 1975,
+      month: 1,
+      day: 1,
+      hour: 0,
+      minute: 0,
+      second: 0,
+      millisecond: 0,
+      offset: 0,
+    });
+    expect(parseRFC3339("1975-12-31T23:59:59-24:59")).toEqual({
+      year: 1975,
+      month: 12,
+      day: 31,
+      hour: 23,
+      minute: 59,
+      second: 59,
+      millisecond: 0,
+      offset: -(24 * 60 + 59),
+    });
+  });
+  describe("invalid", () => {
+    test("month", () => {
+      expect(parseRFC3339("2000-00-01T00:00:00Z")).toBeNull();
+      expect(parseRFC3339("2000-13-01T00:00:00Z")).toBeNull();
+    });
+    test("day", () => {
+      expect(parseRFC3339("2000-01-00T00:00:00Z")).toBeNull();
+      expect(parseRFC3339("1975-08-32T00:00:00Z")).toBeNull();
+    });
+    test("hour", () => {
+      expect(parseRFC3339("0000-01-01T24:00:00Z")).toBeNull();
+    });
+    test("minute", () => {
+      expect(parseRFC3339("0000-01-01T00:60:00Z")).toBeNull();
+    });
+    test("second", () => {
+      expect(parseRFC3339("0000-01-01T00:00:60Z")).toBeNull();
+    });
+  });
+});
+
+describe("format", () => {
+  test("second", () => {
+    expect(
+      formatRFC3339({
+        year: 2025,
+        month: 1,
+        day: 2,
+        hour: 3,
+        minute: 4,
+        second: 5,
+        millisecond: 600,
+        offset: 0,
+      }),
+    ).toBe("2025-01-02T03:04:05.600Z");
+  });
+});
---
+++ src/string/rfc3339.ts
@@ -0,0 +1,61 @@
+import type { DateTimeObject } from "../datetime";
+import {
+  HOURS_IN_DAY,
+  MINUTES_IN_HOUR,
+  MONTHS_IN_YEAR,
+  SECONDS_IN_MINUTE,
+  daysInMonth,
+  type Month,
+} from "../number";
+import { dateToString, timeToString } from "../string";
+import { formatOffset, parseOffset } from "./offset";
+
+// https://www.rfc-editor.org/info/rfc3339
+
+const RFC3339Re =
+  /^(\d{4})-(\d\d)-(\d\d)[T ](\d\d):(\d\d):(\d\d)((?:\.\d+)?)(Z|[+-]\d\d:\d\d)$/;
+
+export const parseRFC3339 = (
+  dt: string,
+): (DateTimeObject & { offset: number }) | null => {
+  const matchResult = RFC3339Re.exec(dt);
+  if (matchResult == null) return null;
+
+  const year = +matchResult[1];
+
+  const month = +matchResult[2];
+  if (month === 0 || month > MONTHS_IN_YEAR) return null;
+
+  const day = +matchResult[3];
+  if (day === 0 || day > daysInMonth(year, month as Month)) return null;
+
+  const hour = +matchResult[4];
+  if (hour >= HOURS_IN_DAY) return null;
+
+  const minute = +matchResult[5];
+  if (minute >= MINUTES_IN_HOUR) return null;
+
+  const second = +matchResult[6];
+  if (second >= SECONDS_IN_MINUTE) return null;
+
+  const ms = matchResult[7].slice(1, 4).padEnd(3, "0");
+  const millisecond = +`${ms}.${matchResult[7].slice(4)}`;
+
+  const offset = parseOffset(matchResult[8], { allowLowerCase: true });
+  if (offset == null) return null;
+
+  return { year, month, day, hour, minute, second, millisecond, offset };
+};
+
+export interface RFC3339FormatOptions {
+  /** @default "T" */
+  deliminator?: string;
+}
+
+export const formatRFC3339 = (
+  dt: DateTimeObject & { offset: number },
+  options?: RFC3339FormatOptions,
+): string => {
+  const delim = options?.deliminator ?? "T";
+  return dateToString(dt) + delim + timeToString(dt) + formatOffset(dt.offset);
+};
