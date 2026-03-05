6f7dc77d92872d99decb61c8c18fc04ac8fb5b78
uzmoi <63585970+uzmoi@users.noreply.github.com>
2026-03-06 04:21:32 +0900

    Add types

--- src/date.ts
+++ src/date.ts
@@ -90,3 +90,10 @@ const daysInMonthArray = [
 export const daysInMonth = (year: number, month: Month): DaysInMonth => {
   return month === 2 && isLeapYear(year) ? 29 : daysInMonthArray[month - 1];
 };
+
+// biome-ignore format: table
+export type DayOfMonth =
+  | (0  | 1  | 2  | 3  | 4  | 5  | 6  | 7  | 8  | 9)
+  | (10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19)
+  | (20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29)
+  | (30 | 31);
--- src/week.ts
+++ src/week.ts
@@ -64,19 +64,30 @@ export const weeksInYear = (
   return weekday === 0 && isLeapYear(year) ? 53 : 52;
 };
 
+// biome-ignore format: table
+export type WeekOfYear =
+  | (0  | 1  | 2  | 3  | 4  | 5  | 6  | 7  | 8  | 9)
+  | (10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19)
+  | (20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29)
+  | (30 | 31 | 32 | 33 | 34 | 35 | 36 | 37 | 38 | 39)
+  | (40 | 41 | 42 | 43 | 44 | 45 | 46 | 47 | 48 | 49)
+  | (50 | 51 | 52 | 53);
+
 /**
  * @returns 1..53
  */
 export const weekOfYear = (
   date: DateObject,
   weekStart = weekStartDefault,
-): number => {
+): WeekOfYear => {
   const weekdayOffset = dayOfWeek({
     year: date.year,
     month: 1,
     day: 1 + DAYS_IN_WEEK - weekStart,
   });
-  return Math.floor((weekdayOffset + dayOfYear(date)) / DAYS_IN_WEEK);
+  return Math.floor(
+    (weekdayOffset + dayOfYear(date)) / DAYS_IN_WEEK,
+  ) as WeekOfYear;
 };
 
 export type WeeksInMonth = 4 | 5 | 6;
