9996d405ddabaa8c546ac4a141e95560dc6269ef
uzmoi <63585970+uzmoi@users.noreply.github.com>
2026-03-13 05:35:52 +0900

    Exclude 0 from DayOfMonth type

--- src/date.ts
+++ src/date.ts
@@ -119,7 +119,7 @@ export const daysInMonth = (year: number, month: Month): DaysInMonth => {
 
 // biome-ignore format: table
 export type DayOfMonth =
-  | (0  | 1  | 2  | 3  | 4  | 5  | 6  | 7  | 8  | 9)
+  | (   | 1  | 2  | 3  | 4  | 5  | 6  | 7  | 8  | 9)
   | (10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19)
   | (20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29)
   | (30 | 31);
