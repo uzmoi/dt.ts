e3576588fc7900b67806b94b21663de74f80d9df
uzmoi <63585970+uzmoi@users.noreply.github.com>
2026-03-13 05:47:57 +0900

    Remove unneeded options from tsdown config

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
   platform: "neutral",
   define: {
     "import.meta.vitest": "null",
   },
-  target: "esnext",
 });
