5f8179e2d8325ccd2916bf2146e7c7433569ed60
uzmoi <63585970+uzmoi@users.noreply.github.com>
2026-03-17 01:33:47 +0900

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ export * from "./datetime.ts";
 export * from "./duration.ts";
 export * from "./interval.ts";
 export * from "./string/index.ts";
-export * from "./string.ts";
 export * from "./time.ts";
+export * from "./week.ts";
