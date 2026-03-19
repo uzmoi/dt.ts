63a589dc209a30a57bda13bfb6b57369ddb0020c
uzmoi <63585970+uzmoi@users.noreply.github.com>
2026-03-19 21:45:03 +0900

    Improve format

--- src/date.ts
+++ src/date.ts
@@ -119,10 +119,10 @@ export const daysInMonth = (year: number, month: Month): DaysInMonth => {
 
 // biome-ignore format: table
 export type DayOfMonth =
-  | (   | 1  | 2  | 3  | 4  | 5  | 6  | 7  | 8  | 9)
-  | (10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19)
-  | (20 | 21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29)
-  | (30 | 31);
+  | (1  | 2  | 3  | 4  | 5  | 6  | 7  | 8  | 9  | 10)
+  | (11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20)
+  | (21 | 22 | 23 | 24 | 25 | 26 | 27 | 28 | 29 | 30)
+  | (31);
 
 export const normalizeCalendarDate = (
   year: number,
