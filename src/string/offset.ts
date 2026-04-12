e689617d3bf4153e30624300b7a7ca6624dd0b3d
uzmoi <63585970+uzmoi@users.noreply.github.com>
2026-04-12 23:48:25 +0900

    Add jsdoc comments

--- src/string/offset.ts
+++ src/string/offset.ts
@@ -2,13 +2,44 @@ import { HOURS_IN_DAY, MINUTES_IN_HOUR } from "../time.ts";
 import { formatInt } from "./utils.ts";
 
 export interface OffsetParseOptions {
+  /**
+   * Whether to allow lowercase 'z'.
+   * @default false
+   */
   allowLowerCase?: boolean;
+
+  /**
+   * Whether to reject '±hh' format.
+   * @default false
+   */
   alwaysFull?: boolean;
+
+  /**
+   * Whether to reject basic format ('±hhmm').
+   * @default false
+   */
   alwaysExtended?: boolean;
 }
 
 const OffsetRegex = /^[+-](\d\d)(?::?(\d\d))?$/;
 
+/**
+ * Parse the offset string into a number in minutes.
+ *
+ * Formats:
+ * - "Z"
+ * - "±hh:mm"
+ * - "±hhmm"
+ * - "±hh"
+ *
+ * @example
+ * ```ts
+ * parseOffset("Z") // 0
+ * parseOffset("+09:00") // 540
+ * parseOffset("-0123") // -83
+ * parseOffset("ｽﾞｲ₍₍(ง˘ω˘)ว⁾⁾ｽﾞｲ") // null
+ * ```
+ */
 export const parseOffset = (
   offset: string,
   options?: OffsetParseOptions,
@@ -36,15 +67,48 @@ export const parseOffset = (
 };
 
 export interface OffsetFormatOptions {
+  /**
+   * Whether to avoid using Z.
+   * @default false
+   */
   neverUseZ?: boolean;
+
+  /**
+   * Whether to allow '±hh' format when minutes is 0.
+   * @default false
+   */
   allowOmitMinutes?: boolean;
+
+  /**
+   * Whether to treat -0 the same as +0.
+   * @default false
+   */
   ignoreNegativeZero?: boolean;
-  /** @default "extended" */
+
+  /**
+   * The output format.
+   * - basic: '±hhmm'
+   * - extended: '±hh:mm'
+   * @default "extended"
+   */
   format?: "extended" | "basic";
 }
 
 const OFFSET_MAX = MINUTES_IN_HOUR * HOURS_IN_DAY;
 
+/**
+ * Format a numeric offset in minutes into a string.
+ *
+ * @throws {RangeError} If offset is out of range.
+ *
+ * @example
+ * ```ts
+ * formatOffset(0) // "Z"
+ * formatOffset(540) // "+09:00"
+ * formatOffset(-540) // "-09:00"
+ * formatOffset(540, { format: "basic" }) // "+0900"
+ * ```
+ */
 export const formatOffset = (
   offset: number,
   options?: OffsetFormatOptions,
