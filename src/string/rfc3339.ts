55ab3fc4de7be903731b8e782bafdce1fe1e2223
uzmoi <63585970+uzmoi@users.noreply.github.com>
2026-04-16 01:47:26 +0900

    refactor: Optimize parseRFC3339 and formatRFC3339 functions for minify

--- src/string/rfc3339.ts
+++ src/string/rfc3339.ts
@@ -9,6 +9,7 @@ import type { OffsetDateTimeObject } from "../datetime.ts";
 import {
   formatTime,
   HOURS_IN_DAY,
+  MILLISECONDS_IN_SECOND,
   MINUTES_IN_HOUR,
   SECONDS_IN_MINUTE,
 } from "../time.ts";
@@ -16,48 +17,56 @@ import { formatOffset, parseOffset } from "./offset.ts";
 
 // https://www.rfc-editor.org/info/rfc3339
 
-const RFC3339Re =
-  /^(\d{4})-(\d\d)-(\d\d)[T ](\d\d):(\d\d):(\d\d)((?:\.\d+)?)(Z|[+-]\d\d:\d\d)$/i;
+const RFC3339RegExp =
+  /^(\d{4})-(\d\d)-(\d\d)[T ](\d\d):(\d\d):(\d\d)(\.\d+)?(Z|[+-]\d\d:\d\d)$/i;
+
+type RFC3339RegExpExecArray = [
+  match: string,
+  year: string,
+  month: string,
+  day: string,
+  hour: string,
+  minute: string,
+  second: string,
+  second_fraction: string | undefined,
+  offset: string,
+];
 
 export const parseRFC3339 = (dt: string): OffsetDateTimeObject | null => {
-  const matchResult = RFC3339Re.exec(dt);
+  const matchResult = RFC3339RegExp.exec(dt) as RFC3339RegExpExecArray | null;
   if (matchResult == null) return null;
 
-  // biome-ignore-start lint/style/noNonNullAssertion: RFC3339Reから自明。
-  // 0000-9999
-  const year = +matchResult[1]!;
+  const year = +matchResult[1];
+  const month = +matchResult[2];
+  const day = +matchResult[3];
+  const hour = +matchResult[4];
+  const minute = +matchResult[5];
+  const second = +matchResult[6];
+  const millisecond = +(matchResult[7] || 0) * MILLISECONDS_IN_SECOND;
+
+  const offset = parseOffset(matchResult[8], { allowLowerCase: true });
+
+  // if文はminifyで折りたたんでもらう。
+
+  if (offset == null) return null;
 
   // 01-12
-  const month = +matchResult[2]!;
   if (month === 0 || month > MONTHS_IN_YEAR) return null;
 
   // 01-28, 01-29, 01-31, 01-31
-  const day = +matchResult[3]!;
   if (day === 0 || day > getDaysInMonth(year, month as Month)) return null;
 
   // 00-23
-  const hour = +matchResult[4]!;
   if (hour >= HOURS_IN_DAY) return null;
 
   // 00-59
-  const minute = +matchResult[5]!;
   if (minute >= MINUTES_IN_HOUR) return null;
 
   // 00-60
   // 正の閏秒として60が許可されている。
-  const second = +matchResult[6]!;
-  if (second > SECONDS_IN_MINUTE) {
-    return null;
-  }
+  if (second > SECONDS_IN_MINUTE) return null;
 
-  const ms = matchResult[7]!.slice(1, 4).padEnd(3, "0");
-  const millisecond = +`${ms}.${matchResult[7]!.slice(4)}`;
-
-  const offset = parseOffset(matchResult[8]!, { allowLowerCase: true });
-  // biome-ignore-end lint/style/noNonNullAssertion: RFC3339Reから自明。
-  if (offset == null) return null;
-
-  // 閏秒は必ず UTC の 23:59:60 に挿入される。
+  // 正の閏秒は必ず UTC の 23:59:60 に挿入される。
   if (second === 60) {
     const t =
       (hour * MINUTES_IN_HOUR + minute - offset + 1) %
@@ -86,6 +95,9 @@ export const formatRFC3339 = (
   dt: OffsetDateTimeObject,
   options?: RFC3339FormatOptions,
 ): string => {
+  const date = formatDate(dt);
   const delim = options?.deliminator ?? "T";
-  return formatDate(dt) + delim + formatTime(dt) + formatOffset(dt.offset);
+  const time = formatTime(dt);
+  const offset = formatOffset(dt.offset);
+  return date + delim + time + offset;
 };
