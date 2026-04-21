21be49752f4815a63ea4d90f99957b2bae7de059
uzmoi <63585970+uzmoi@users.noreply.github.com>
2026-04-22 01:43:09 +0900

    feat: Add Duration class

---
+++ src/duration.test.ts
@@ -0,0 +1,7 @@
+import { describe } from "vitest";
+// biome-ignore lint/correctness/noUnusedImports: todo
+import { Duration as _ } from "./duration.ts";
+
+describe.todo("parse");
+
+describe.todo("format");
--- src/duration.ts
+++ src/duration.ts
@@ -1,3 +1,5 @@
+import { todo } from "@uzmoi/ut/ils";
+
 export interface DurationObject {
   years: number;
   months: number;
@@ -6,4 +8,28 @@ export interface DurationObject {
   minutes: number;
   seconds: number;
   milliseconds: number;
+}
+
+export class Duration implements DurationObject {
+  constructor(
+    readonly years: number,
+    readonly months: number,
+    readonly days: number,
+    readonly hours: number,
+    readonly minutes: number,
+    readonly seconds: number,
+    readonly milliseconds: number,
+  ) {}
+
+  toString(): string {
+    return todo("formatDuration");
+  }
+
+  toJSON(): string {
+    return this.toString();
+  }
+
+  valueOf(): never {
+    throw new Error(todo("error message"));
+  }
 }
